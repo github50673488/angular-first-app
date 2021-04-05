46348554a1159261d1363ce248f5354cc991f25d
liuguojun <gj.liu@sj-sol.com>
2021-04-05 17:36:34 +0900

    2.2.1 HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,10 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
   title = 'first-app';
 
   // 定义教师数组
