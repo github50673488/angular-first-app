6453baac2472c0c89948a35bf89ed122a6078d65
liuguojun <gj.liu@sj-sol.com>
2021-04-05 17:03:11 +0900

    2.1.3 ngFor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -18,6 +18,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+    fixture.autoDetectChanges(); // 启用angular的自动变更检测机制，自动对V层中的数据进行渲染 在ng t的情况下，angular将是否启用这个自动感知组件中的值发生变化
   });
 
 
