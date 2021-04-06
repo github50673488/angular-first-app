e18274ed234c9bb550aa714af1d59131955bee7f
liuguojun <gj.liu@sj-sol.com>
2021-04-06 09:52:56 +0900

    2.4.3 插值与模板表达式

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-edit',
@@ -8,12 +9,28 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class EditComponent implements OnInit {
 
-  constructor(private activeRoute: ActivatedRoute) {
+  public teacher: any;
+
+  constructor(private activeRoute: ActivatedRoute
+    ,         private httpClient: HttpClient
+  ) {
   }
 
   ngOnInit(): void {
     const id = this.activeRoute.snapshot.params.id;
     console.log('获取到的路由参数id值为', id);
+
+    // 拼接请求URL
+    const url = 'http://angular.api.codedemo.club:81/teacher/' + id;
+    // 发起请求，成功时并打印请求结果，失败时打印失败结果
+    this.httpClient.get(url)
+      .subscribe((data) => {
+        console.log('成功', data);
+        this.teacher = data;
+      }, (error) => {
+        console.log('失败', error);
+      });
+
   }
 
 }
