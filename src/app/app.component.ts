3bab8808b3fc07aa980b4f1c33d3f849f498985b
liuguojun <gj.liu@sj-sol.com>
2021-04-06 10:34:30 +0900

    2.5 删除教师

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+type Teacher = {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  sex: true
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
 
   constructor(private httpClient: HttpClient) {
   }
@@ -15,7 +24,8 @@ export class AppComponent implements OnInit {
 
   // 定义教师数组
   // 初始化教师数组
-  teachers: any[] = []; // 在这里不加类型（: any[]）模版会出错
+  // teachers: any[] = []; // 在这里不加类型（: any[]）模版会出错
+  teachers = [] as Teacher[];
 
   ngOnInit(): void {
     // this.httpClient.get('assets/teacher-all.json')
@@ -25,13 +35,28 @@ export class AppComponent implements OnInit {
     //   this.httpClient.get('assets/teacher-all.json')从http获取任意类型的东西
 
 
-    this.httpClient.get<[]>('http://angular.api.codedemo.club:81/teacher')
-      .subscribe((teachers) => {
-        console.log(teachers);
-        this.teachers = teachers;
-      });
+    // this.httpClient.get<[]>('http://angular.api.codedemo.club:81/teacher')
+    this.loadFromHttp();
 
 
   }
 
+  private loadFromHttp(): void {
+    this.httpClient.get<Teacher[]>('http://angular.api.codedemo.club:81/teacher')
+      .subscribe((teachers) => {
+        console.log(teachers);
+        this.teachers = teachers;
+      });
+  }
+
+  onDelete(id: number): void {
+
+    const url = `http://angular.api.codedemo.club:81/teacher/${id}`; // 我们在此使用是`而非' `123${a}` 等价于'123' + a
+    this.httpClient.delete(url)
+      .subscribe(data => {
+          console.log('删除成功');
+          this.loadFromHttp();
+        },
+        error => console.log('删除失败', error));
+  }
 }
